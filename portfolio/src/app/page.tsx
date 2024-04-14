4bfa4b218d604e26936bc1868523d7e5de943739
Muhammad-AliHaider <107212677+Muhammad-AliHaider@users.noreply.github.com>
2024-04-15 01:14:44 +0500

    changes_deployable

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,17 +1,32 @@
-"use client";
+'use client';
 
-import Image from 'next/image'
-import Sidebar from '@/components/sidebar/sidebar'
-import { redirect } from 'next/dist/server/api-utils';
-import styles from './page.module.css'
-import { useEffect } from 'react';
-import { useRouter } from 'next/router'
+import React, {useState} from 'react';
 
-export default function Home() {
-   const router = useRouter();
+import ContentFrame from "@/components/ContentFrame/contentFrame";
+import PicFrame from "@/components/pictureFrame/picFrame";
+import Sidebar from "@/components/sidebar/sidebar";
+import cn from 'classnames';
+import styles from './style.module.sass';
 
-   
-    return router.push('/home');
+export default  function Homepage(){
 
-  
+    const [activeTab, setActiveTab] = useState('tab1');
+
+    return (
+        <div className={cn(styles.bgContainer)} > 
+            <div className = {cn(styles.centerDiv)}>
+            <Sidebar activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <PicFrame/>
+            <ContentFrame  tabActive={activeTab} />
+            </div>
+            <div className= {cn(styles.enable404)}>
+                <h1 className={cn(styles.contentHeading)}>
+                    404
+                </h1>
+                <p className={cn(styles.contentText)}>
+                    Sorry the page is not available for this screen size, kindly Zoom out 
+                </p>
+            </div>
+        </div>
+    );
 }
